0b46e7f61c4b78dcde93622ef24f2f13bf0f6200
Gareth618 <bloggareth@gmail.com>
2022-04-04 17:58:35 +0300

    Created SignIn and SignUp components

--- web-frontend/src/index.js
+++ web-frontend/src/index.js
@@ -1,9 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
 import './index.css';
+import SignIn from './SignIn';
+import SignUp from './SignUp';
+
+function PageSwitcher() {
+  const [page, setPage] = React.useState('sign-in', 'sign-up');
+  return (
+    <>
+      <button
+        className="theme-switcher"
+        onClick={() => setPage(page === 'sign-in' ? 'sign-up' : 'sign-in')}
+      >
+        switch
+      </button>
+      {page === 'sign-in' ? <SignIn /> : <SignUp />}
+    </>
+  );
+}
 
 ReactDOM.render(
   <React.StrictMode>
+    <PageSwitcher />
   </React.StrictMode>,
   document.getElementById('root')
 );
