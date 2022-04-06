55fc58ea9d51c6b6c58c8fd8af5ece2ba9073ee2
AndreeaJinga <andreea.jinga@yahoo.com>
2022-04-06 17:36:13 +0300

    container

--- web-frontend/src/components/SignIn/SignIn.js
+++ web-frontend/src/components/SignIn/SignIn.js
@@ -5,6 +5,9 @@ import Background from 'assets/background.svg';
 export default function SignIn() {
   return (
     <>
+      <div className={styles.container}>
+        dsfsdfsfssssssssssssssssssssssss <br />
+      </div>
       <img src={Background} className={styles.bg} draggable="false" />
     </>
   );
