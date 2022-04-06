23b45dcb0d49d9690ba80e2a9fd63719b4dae8b9
AndreeaJinga <andreea.jinga@yahoo.com>
2022-04-06 17:47:29 +0300

    input username+password

--- web-frontend/src/components/SignIn/SignIn.js
+++ web-frontend/src/components/SignIn/SignIn.js
@@ -1,13 +1,23 @@
 import React from 'react';
 import styles from './SignIn.module.css';
 import Background from 'assets/background.svg';
+import { TextField } from '@mui/material';
 
 export default function SignIn() {
   return (
     <>
-      <div className={styles.container}>
-        dsfsdfsfssssssssssssssssssssssss <br />
-      </div>
+      <form className={styles.container}>
+        <TextField
+          label="Username or email address"
+          variant="standard"
+          style={{ display: 'block', width: 300 }}
+        />
+        <TextField
+          label="Password"
+          type="password"
+          variant="standard"
+        />
+      </form>
       <img src={Background} className={styles.bg} draggable="false" />
     </>
   );
