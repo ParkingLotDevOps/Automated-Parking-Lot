de0cb1583aebbea7c25756d928977b7bee6045d8
iacobdariaroxana <iacobdariaroxana@gmail.com>
2022-04-21 12:06:32 +0300

    mobile app

--- Mobile/Front-end/App.js
+++ Mobile/Front-end/App.js
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, Image, Button,
     useWindowDimensions } from 'react-native';
@@ -73,3 +74,38 @@ const styles = StyleSheet.create({
         height: 56
     },
 });
+=======
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import Login from './components/Login';
+import SignUp from './components/SignUp';
+import SignUpPhone from './components/SignUpPhone';
+import SignUpVerification from './components/SignUpVerification';
+import SignUpPassword from './components/SignUpPassword';
+import SignUpAddCar from './components/SignUpAddCar';
+import Car from './components/Car';
+import Index from './components/Index';
+import LogoMap from './components/LogoMap';
+import LogoPayment from './components/LogoPayment';
+const Stack = createStackNavigator();
+
+export default function App() {
+	return (
+		<NavigationContainer>
+			<Stack.Navigator initialRouteName='Index'  screenOptions={{headerShown: false}}>
+				<Stack.Screen name='Index' component={Index}/>
+				<Stack.Screen name='Login' component={Login} />
+				<Stack.Screen name='SignUp' component={SignUp} />
+				<Stack.Screen name='SignUpPhone' component={SignUpPhone} />
+				<Stack.Screen name='SignUpVerification' component={SignUpVerification} />
+				<Stack.Screen name='SignUpPassword' component={SignUpPassword} />
+				<Stack.Screen name='SignUpAddCar' component={SignUpAddCar} />
+				<Stack.Screen name='Car' component={Car}/>
+				<Stack.Screen name='LogoMap' component={LogoMap}/>
+				<Stack.Screen name='LogoPayment' component={LogoPayment}/>
+			</Stack.Navigator>
+		</NavigationContainer>
+	);
+}
+>>>>>>> Stashed changes
--- Mobile/Front-end/babel.config.js
+++ Mobile/Front-end/babel.config.js
@@ -2,5 +2,15 @@ module.exports = function(api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
+<<<<<<< Updated upstream
   };
 };
+=======
+    env: {
+      production: {
+        plugins: ['react-native-paper/babel'],
+      },
+    },
+  };
+};
+>>>>>>> Stashed changes
