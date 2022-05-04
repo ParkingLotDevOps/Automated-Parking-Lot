a22a93f3718d1c8af641a995a8dc35cdeac0a6cc
AndreeaJinga <andreea.jinga@yahoo.com>
2022-05-04 13:42:04 +0300

    started edit parking lot

---
+++ web-frontend/src/components/EditParkingLot/EditParkingLot.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Background from 'assets/background.svg';
+import styles from './EditParkingLot.module.css';
+
+export default function EditParkingLot() {
+  return (
+    <h1>Parking Lot Name 1</h1>
+  );
+};
+  
