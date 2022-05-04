c13a9130afe4911239aedf57eb3c9b35e141e7be
BITDEFENDER\mapopovici <mapopovici@bitdefender.com>
2022-05-04 18:17:30 +0300

    test cors

--- spl/src/main/java/b3/spl/splb/SplbApplication.java
+++ spl/src/main/java/b3/spl/splb/SplbApplication.java
@@ -9,6 +9,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.ArrayList;
 
@@ -37,4 +39,5 @@ public class SplbApplication {
     PasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
     }
+
 }
