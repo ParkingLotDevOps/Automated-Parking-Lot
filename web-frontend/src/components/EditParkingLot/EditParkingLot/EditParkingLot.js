3679259dfc0298e6550a308084475e823e5b8c24
Gareth618 <bloggareth@gmail.com>
2022-06-01 21:13:46 +0300

    added edit parking lot name check

--- web-frontend/src/components/EditParkingLot/EditParkingLot/EditParkingLot.js
+++ web-frontend/src/components/EditParkingLot/EditParkingLot/EditParkingLot.js
@@ -2,6 +2,7 @@ import React from 'react';
 import styles from './EditParkingLot.module.css';
 import { useParams, useNavigate } from 'react-router-dom';
 import { EditParkingLotPanel, EditParkingLotGrid, Sidebar } from 'components';
+import { getUserData } from 'hooks';
 
 export default function EditParkingLot() {
   const navigate = useNavigate();
@@ -15,13 +16,29 @@ export default function EditParkingLot() {
   }
 
   const { name } = useParams();
+  const [data, setData] = React.useState(null);
+  const getParkingLot = async () => {
+    const lots = (await getUserData()).parkingLots;
+    const data = lots.find(lot => lot.name === name.replace(/-/g, ' '));
+    if (data == null) {
+      navigate('/dashboard');
+    }
+    setData(data);
+  };
+  React.useEffect(() => {
+    getParkingLot();
+  }, []);
+
+  if (data == null) {
+    return <></>;
+  }
   return (
     <>
       <Sidebar />
       <main className={styles.main}>
         <h1 className={styles.name}>{name.replace(/-/g, ' ')}</h1>
         <EditParkingLotPanel />
-        <EditParkingLotGrid spotsCount={17} />
+        <EditParkingLotGrid spotsCount={data.spots.length} />
       </main>
     </>
   );
