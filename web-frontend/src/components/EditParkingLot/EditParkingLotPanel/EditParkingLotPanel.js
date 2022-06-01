ae46c85bcbf1385ac64ed0cc11a3cbd6634383ca
lizzzu <lizudim@yahoo.com>
2022-06-01 18:19:29 +0300

    edit and add new parking lot colors

--- web-frontend/src/components/EditParkingLot/EditParkingLotPanel/EditParkingLotPanel.js
+++ web-frontend/src/components/EditParkingLot/EditParkingLotPanel/EditParkingLotPanel.js
@@ -3,6 +3,24 @@ import { FaCamera } from 'react-icons/fa';
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
 import styles from './EditParkingLotPanel.module.css';
+import { createTheme} from '@mui/material/styles';
+import { ThemeProvider } from '@mui/material/styles';
+
+const textFieldTheme = createTheme({
+  palette: {
+    primary: {
+      main: '#E8E4E6'
+    }
+  }
+});
+
+const buttonTheme = createTheme({
+  palette: {
+    primary: {
+      main: '#E16162'
+    }
+  }
+});
 
 export default function EditParkingLotPanel() {
   return (
@@ -18,48 +36,58 @@ export default function EditParkingLotPanel() {
         <div className={styles.right}>
           <div className={styles.col}>
             <div className={styles.row}>
+              <ThemeProvider theme={textFieldTheme}>
+                <TextField
+                  label="Name"
+                  type="text"
+                  InputLabelProps={{
+                    shrink: true,
+                  }}
+                  sx={{ input: { color: '#E8E4E6' } }}
+                  variant="filled"
+                />
+              </ThemeProvider>
+              <ThemeProvider theme={textFieldTheme}>
+                <TextField
+                  label="Pricing"
+                  type="number"
+                  InputLabelProps={{
+                    shrink: true,
+                  }}
+                  sx={{ input: { color: '#E8E4E6' } }}
+                  variant="filled"
+                />
+              </ThemeProvider>
+            </div>
+            <ThemeProvider theme={textFieldTheme}>
               <TextField
-                label="Name"
+                label="Location"
                 type="text"
                 InputLabelProps={{
                   shrink: true,
                 }}
-                sx={{ input: { color: 'dodgerblue' } }}
+                sx={{ input: { color: '#E8E4E6' } }}
                 variant="filled"
               />
+            </ThemeProvider>
+          </div>
+          <div className={styles.col}>
+            <ThemeProvider theme={textFieldTheme}>
               <TextField
-                label="Pricing"
+                label="Number of Spots Available"
                 type="number"
                 InputLabelProps={{
                   shrink: true,
                 }}
-                sx={{ input: { color: 'dodgerblue' } }}
+                sx={{ input: { color: '#E8E4E6' } }}
                 variant="filled"
               />
-            </div>
-            <TextField
-              label="Location"
-              type="text"
-              InputLabelProps={{
-                shrink: true,
-              }}
-              sx={{ input: { color: 'dodgerblue' } }}
-              variant="filled"
-            />
-          </div>
-          <div className={styles.col}>
-            <TextField
-              label="Number of Spots Available"
-              type="number"
-              InputLabelProps={{
-                shrink: true,
-              }}
-              sx={{ input: { color: 'dodgerblue' } }}
-              variant="filled"
-            />
-            <Button variant="contained" sx={{ height: 55 }}>
-              Save Changes
-            </Button>
+            </ThemeProvider>
+            <ThemeProvider theme={buttonTheme}>
+              <Button variant="contained" sx={{ height: 55 }}>
+                Save Changes
+              </Button>
+            </ThemeProvider>
           </div>
         </div>
       </div>
